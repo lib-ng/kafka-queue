4cd0acb345feb23553f6cc3b7ff03952e1507ce8
Emmanuel Chukwu <emmanuel.chukwu@gmail.com>
2022-10-15 17:44:33 +0100

    first commit

---
+++ src/KafkaConnector.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kafka;
+
+use Illuminate\Queue\Connectors\ConnectorInterface;
+
+class KafkaConnector implements ConnectorInterface
+{
+    
+    public function connect(array $config)
+    {
+        $producer = $this->createProducer($config);
+        $consumer = $this->createConsumer($config);  
+        return new KafkaQueue($producer, $consumer);
+    }
+
+    public function createProducer(array $config)
+    {          
+        $conf = $this->setCommonParameters($config);
+        $conf->set('message.send.max.retries', 5);
+        $producer = new \RdKafka\Producer($conf);
+        $producer->setLogLevel(LOG_DEBUG);
+        return $producer;
+    }
+    public function createConsumer(array $config)
+    {          
+        $conf = $this->setCommonParameters($config);
+        $conf->set('group.id', $config['group_id']);
+        $conf->set('auto.offset.reset', 'earliest');
+
+        $consumer = new \RdKafka\KafkaConsumer($conf);
+        
+        $topic = $config['queue'];
+        $consumer->subscribe([$topic]);
+        dump("subscribed to topic - $topic");
+        return $consumer;
+    }
+
+    public function setCommonParameters(array $config)
+    {
+        $conf = new \RdKafka\conf();
+        $conf->set('bootstrap.servers', $config['bootstrap_servers']);
+        $conf->set('security.protocol', $config['security_protocol']);
+        $conf->set('sasl.mechanisms', $config['sasl_mechanisms']);
+        $conf->set('sasl.username', $config['sasl_username']);
+        $conf->set('sasl.password', $config['sasl_password']);  
+        return $conf;
+    }
+
+}
---
+++ src/KafkaQueue.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Kafka;
+
+use Illuminate\Queue\Queue;
+use Illuminate\Contracts\Queue\Queue as QueueContract;
+
+class KafkaQueue extends Queue implements QueueContract
+{
+    protected $producer, $consumer;
+
+    public function __construct($producer, $consumer)
+    {
+        $this->producer = $producer;
+        $this->consumer = $consumer;
+    }
+
+    public function later($delay, $job, $data = '', $queue = null)
+    {
+
+    }
+   
+    public function pop($queue = null)
+    {     
+        try{
+            $message = $this->consumer->consume(30 * 1000);
+
+            switch($message->err){
+                case RD_KAFKA_RESP_ERR_NO_ERROR:
+                    $job = unserialize($message->payload);
+                    $job->handle();
+                    break;
+                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                    dump("No more messages; will wait for more.");
+                    break;
+                case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                    dump("Time out.");
+                    break;                
+                default:
+                    throw new \Exception($message->errstr(), $message->err);
+                    break;
+            } 
+        }
+        catch (\Exception $e) {
+            dump($e.getMessage());
+        }
+    }
+
+
+    public function push($job, $data = '', $queue = null)
+    {
+        $topic_name = $queue ?? config('queue.connections.kafka.queue');
+        $kafka_topic = $this->producer->newTopic($topic_name);
+        $a = $kafka_topic->produce(RD_KAFKA_PARTITION_UA, 0, serialize($job));
+
+        $this->producer->poll(0);
+        while ($this->producer->getOutQLen() > 0) {
+            $this->producer->poll(50);
+        }
+        echo "Successful produced on topic - $topic_name" . PHP_EOL;
+
+        $this->producer->flush(1000);
+    }
+
+    public function pushRaw($payload, $queue = null, array $options = [])
+    {
+
+    }
+
+    public function size($queue = null)
+    {     
+          
+    }
+
+
+
+}
